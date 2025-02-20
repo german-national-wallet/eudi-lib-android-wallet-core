f71abea5297f450fbaca003d8dfbabdde5763411
Jose Luis Crisostomo <12452269+simplekjl@users.noreply.github.com>
2025-02-20 16:00:36 +0100

    Store refresh token (#44)
    * batch credential and delete pid after presentation
    
    * merge with latest changes in master
    
    * fixed tests
    
    * added tests for Presentation of documents when deletion happens
    
    * clean up
    
    * revert changes, no need with latest changes in master
    
    * revert formatting
    
    * clean up
    
    * added single issuance
    
    * remove document id
    
    * storing refresh token wip
    
    * storing refresh token wip
    
    * adding saving refresh token also in RI flow
    
    * Added doc comment
    
    * fixed tests
    
    * Rename RefreshToken -> RefreshTokenReceived
    
    * Rename AuthState -> OnRefreshTokenReceived
    
    * Rename value -> refreshToken
    
    * remove nullable statement for refreshToken

--- wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/IssueEvent.kt
+++ wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/IssueEvent.kt
@@ -17,6 +17,7 @@ package eu.europa.ec.eudi.wallet.issue.openid4vci
 
 import com.android.identity.crypto.Algorithm
 import com.android.identity.securearea.KeyUnlockData
+import eu.europa.ec.eudi.openid4vci.RefreshToken
 import eu.europa.ec.eudi.wallet.document.CreateDocumentSettings
 import eu.europa.ec.eudi.wallet.document.DeferredDocument
 import eu.europa.ec.eudi.wallet.document.DocumentId
@@ -114,6 +115,16 @@ sealed interface IssueEvent : OpenId4VciResult {
     data class DocumentDeferred(val document: DeferredDocument) :
         IssueEvent,
         DocumentDetails by DocumentDetails(document)
+
+    /**
+     * Authorization with refresh token
+     * This token can or not be provided by the Issuer, if present should be stored
+     * for future credential issuance
+     *
+     * @property refreshToken the refresh token given by the authorization server
+     */
+
+    data class AuthorizationWithRefreshToken(val refreshToken: RefreshToken?) : IssueEvent
 
     companion object {
         internal fun failure(
