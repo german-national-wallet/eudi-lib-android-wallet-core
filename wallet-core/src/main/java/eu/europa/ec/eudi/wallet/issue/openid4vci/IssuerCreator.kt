b7e592118ddc23288cf206551d25fd2731f12d86
Jose Luis Crisostomo <12452269+simplekjl@users.noreply.github.com>
2025-03-13 18:17:50 +0100

    Fetch credentials using refresh token (#50)
    * wip refresh token
    
    * wip, with Crypto lib
    
    * wip persist private dpop key
    
    * remove todos and refresh token interactor
    
    * wip, refresh done after pid deletion
    
    * update DefaultOpenId4VciManager with some refactor
    
    * fix batch to 5, currently is 42 from PID
    
    * updating logic in upper layers
    
    * improve naming for keys
    
    * clean up pem
    
    * removed unused code
    
    * clean up signer
    
    * clean up signer
    
    * Added more meaningfull names to dpop key methods
    
    * fix Sd-jwt issuance after refactor
    
    * Added test cases for hardware key storage
    
    * added test coverage for refresh token cases
    
    * Clean up in Config Wallet Impl for DEv and demo
    
    * ignoring tests
    
    * WD-426 improved key generation for vci configuration
    
    * WD-426 unused imports removed
    
    * WD-426 provided jwssigner usage for attestation cases
    
    * WD-426 else condition if refresh token is empty
    
    * WD-426 refactoring
    
    ---------
    
    Co-authored-by: Daniyal Idrees <daniyal.idrees@sprind.org>

--- wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/IssuerCreator.kt
+++ wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/IssuerCreator.kt
@@ -16,6 +16,7 @@
 
 package eu.europa.ec.eudi.wallet.issue.openid4vci
 
+import com.android.identity.crypto.Algorithm
 import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.JWSSigner
 import com.nimbusds.jose.jwk.Curve
@@ -68,7 +69,7 @@ internal class IssuerCreator(
         offer: Offer,
         attestationJWT: SignedJWT,
         jwsAlgorithm: JWSAlgorithm,
-        durationInMin : Int,
+        durationInMin: Int,
         type: String,
         jwsSigner: JWSSigner
     ): Issuer {
@@ -93,7 +94,9 @@ internal class IssuerCreator(
      * @param credentialConfigurationIdentifiers The [CredentialConfigurationIdentifier]s.
      * @return The [Issuer].
      */
-    suspend fun createIssuer(credentialConfigurationIdentifiers: List<CredentialConfigurationIdentifier>): Issuer {
+    suspend fun createIssuer(
+        credentialConfigurationIdentifiers: List<CredentialConfigurationIdentifier>,
+    ): Issuer {
         return Issuer.makeWalletInitiated(
             config = config.toOpenId4VCIConfig(),
             credentialIssuerId = CredentialIssuerId(config.issuerUrl).getOrThrow(),
@@ -138,7 +141,10 @@ internal class IssuerCreator(
             authFlowRedirectionURI = URI.create(authFlowRedirectionURI),
             keyGenerationConfig = KeyGenerationConfig(Curve.P_256, 2048),
             credentialResponseEncryptionPolicy = CredentialResponseEncryptionPolicy.SUPPORTED,
-            dPoPSigner = if (useDPoPIfSupported) JWSDPoPSigner().getOrNull() else null,
+            dPoPSigner = JWSDPoPSigner(
+                keyProvider = config.privateKeySource,
+                algorithm = Algorithm.ES256
+            ).getOrThrow(),
             parUsage = when (parUsage) {
                 OpenId4VciManager.Config.ParUsage.IF_SUPPORTED -> ParUsage.IfSupported
                 OpenId4VciManager.Config.ParUsage.REQUIRED -> ParUsage.Required
@@ -152,7 +158,7 @@ internal class IssuerCreator(
     private fun OpenId4VciManager.Config.toOpenId4VCIConfigWithAttestation(
         attestationJWT: SignedJWT,
         jwsAlgorithm: JWSAlgorithm,
-        durationInMin : Int,
+        durationInMin: Int,
         type: String,
         jwsSigner: JWSSigner,
     ): OpenId4VCIConfig {
@@ -169,7 +175,10 @@ internal class IssuerCreator(
             authFlowRedirectionURI = URI.create(authFlowRedirectionURI),
             keyGenerationConfig = KeyGenerationConfig(Curve.P_256, 2048),
             credentialResponseEncryptionPolicy = CredentialResponseEncryptionPolicy.SUPPORTED,
-            dPoPSigner = if (useDPoPIfSupported) JWSDPoPSigner().getOrNull() else null,
+            dPoPSigner = JWSDPoPSigner(
+                keyProvider = config.privateKeySource,
+                algorithm = Algorithm.ES256
+            ).getOrThrow(),
             parUsage = when (parUsage) {
                 OpenId4VciManager.Config.ParUsage.IF_SUPPORTED -> ParUsage.IfSupported
                 OpenId4VciManager.Config.ParUsage.REQUIRED -> ParUsage.Required
