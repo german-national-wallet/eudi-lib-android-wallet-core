12c99f331f5787ceb9a6b3195c8e66be7b127cfc
Tobias Thierer <tobias.thierer@eudi.sprind.org>
2025-10-09 20:50:14 +0200

    revert some unecessary changes from the previous commit.
    This commit was created by running:
    
    git checkout e7a3568b2459367b1145f39715932b55f3ca9d90 wallet-core/proguard-rules.pro wallet-core/src/main/AndroidManifest.xml wallet-core/src/main/java/eu/europa/ec/eudi/wallet/EudiWallet.kt wallet-core/src/main/java/eu/europa/ec/eudi/wallet/EudiWalletConfig.kt wallet-core/src/main/java/eu/europa/ec/eudi/wallet/internal/KtorHttpClientFactoryExtensions.kt wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/IssueEvent.kt  wallet-core/src/main/java/eu/europa/ec/eudi/wallet/transfer/openId4vp/OpenId4VpManager.kt wallet-core/src/main/java/eu/europa/ec/eudi/wallet/transfer/openId4vp/OpenId4VpResponse.kt wallet-core/src/main/java/eu/europa/ec/eudi/wallet/transfer/openId4vp/ProcessedOpenId4VpRequest.kt wallet-core/src/test/java/eu/europa/ec/eudi/wallet/issue/openid4vci/DeferredIssuanceContextTest.kt wallet-core/src/test/java/eu/europa/ec/eudi/wallet/issue/openid4vci/IssuerAuthorizationTest.kt wallet-core/src/test/java/eu/europa/ec/eudi/wallet/sample/MDLAllFieldsTest.kt wallet-core/src/test/java/eu/europa/ec/eudi/wallet/sample/PIDMandatoryFieldsTest.kt wallet-core/src/test/java/eu/europa/ec/eudi/wallet/sample/util/BaseTest.kt wallet-core/src/test/java/eu/europa/ec/eudi/wallet/sample/util/CBORTestUtil.kt wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/Utils.kt wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/OfferResolver.kt
    
    The changes to the license header of the following file were also reverted: wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/IssuerCreator.kt

--- wallet-core/src/test/java/eu/europa/ec/eudi/wallet/issue/openid4vci/DeferredIssuanceContextTest.kt
+++ wallet-core/src/test/java/eu/europa/ec/eudi/wallet/issue/openid4vci/DeferredIssuanceContextTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 European Commission
+ * Copyright (c) 2024-2025 European Commission
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@ import eu.europa.ec.eudi.openid4vci.DeferredIssuanceContext
 import eu.europa.ec.eudi.openid4vci.RefreshToken
 import io.mockk.every
 import io.mockk.mockk
-import org.junit.Ignore
 import java.time.Instant
 import kotlin.test.Test
 import kotlin.test.assertFalse
