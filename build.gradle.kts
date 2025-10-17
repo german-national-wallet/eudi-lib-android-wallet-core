ec2e0c6159211bea820c0e0d879073d9f4be9163
Tobias Thierer <tobias.thierer@eudi.sprind.org>
2025-10-17 21:04:17 +0200

    Explicitly use JavaVersion.VERSION_17 in core/build.gradle.kts
    I don't expect this to be needed because other moduels depend on
    :core:wallet-core and because other modules already use VERSION_17,
    but for some reason CI failed on the main branch after the submodule
    was added, and, empirically, this commit will probably fix it.

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,15 @@ android {
     buildFeatures {
         buildConfig = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
 }
 
 /*
