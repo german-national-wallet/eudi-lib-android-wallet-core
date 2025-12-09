fb5f95d653ba901f89dbb785e86cb75637b93a76
Tobias Thierer <tobias.thierer@eudi.sprind.org>
2025-12-09 22:26:58 +0100

    depend on openid4vci (source) project instead of corresponding library

--- wallet-core/build.gradle.kts
+++ wallet-core/build.gradle.kts
@@ -153,7 +153,7 @@ dependencies {
     api(libs.eudi.iso18013.data.transfer)
     // OpenID4VCI
     // EUID-added
-    api(libs.eudi.lib.jvm.openid4vci.kt.upstream)
+    api(project(LibraryModule.Openid4vci.path))
     // multipaz library
     api(libs.multipaz.android) {
         exclude(group = "org.bouncycastle")
