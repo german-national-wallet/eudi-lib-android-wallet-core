d62aee5bc5a3edfe2d478c0b0b5674380008bc3a
Tobias Thierer <tobias.thierer@eudi.sprind.org>
2026-03-10 18:16:53 +0100

    [WD-2107] Remove code dealing with cNonce

--- wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/DefaultOpenId4VciManager.kt
+++ wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/DefaultOpenId4VciManager.kt
@@ -484,9 +484,6 @@ internal class DefaultOpenId4VciManager(
                     listener(IssueEvent.AuthorizationWithRefreshToken(request.authorizedRequest.refreshToken))
                 }
 
-                val cNonce = (authorizedRequest as AuthorizedRequest.ProofRequired).cNonce
-                listener(IssueEvent.CNonceAvailable(cNonce))
-
                 val issuedDocumentIds = mutableListOf<DocumentId>()
                 ProcessResponse(
                     documentManager = documentManager,
